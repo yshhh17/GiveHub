2c38add597d322ed397162afdd6315094a3935e2
yshhh17 <iamyashtiwari28@gmail.com>
2026-04-16 23:56:37 +0530

    doing cleanups

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import Hero from '../components/home/Hero';
 import Features from '../components/home/Features';
 import CTA from '../components/home/CTA';
-import Footer from '../components/Footer';
 
 const Home = () => {
   return (
